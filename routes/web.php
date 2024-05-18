be796266c44a36a6aac59c9ed30b4f440faf237f
da-imran <ios.imrannn@gmail.com>
2024-05-18 23:24:38 +0800

    :sparkles: Add new images files

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Main Route
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/utama', function () {
     return view('index.index');
 });
+
+// Info Korporat Tab Menu
 Route::get('/info-korporat/perutusan-imam-besar', function () {
     return view('corporate.perutusanImamBesar');
 });
@@ -39,3 +41,32 @@ Route::get('/info-korporat/direktori-kakitangan', function () {
     return view('corporate.direktoriKakitangan');
 });
 
+// Informasi Tab Menu
+Route::get('/informasi/berita-semasa', function () {
+    return view('information.beritaSemasa');
+});
+Route::get('/informasi/kemudahan', function () {
+    return view('information.kemudahan');
+});
+Route::get('/informasi/takwim', function () {
+    return view('information.takwim');
+});
+Route::get('/informasi/kutipan-tabung-masjid', function () {
+    return view('information.kutipanTabungMasjid');
+});
+
+// Muat Turun Tab Menu
+Route::get('/muat-turun/jadual-kuliah', function () {
+    return view('download.jadualKuliah');
+});
+Route::get('/muat-turun/nota-kuliah', function () {
+    return view('download.notaKuliah');
+});
+Route::get('/muat-turun/borang', function () {
+    return view('download.borang');
+});
+
+// Hubungi Kami Tab Menu
+Route::get('/hubungi-kami', function () {
+    return view('contact');
+});
