e8d6ab548ff7170274cd51abca1280d67aa1149e
da-imran <ios.imrannn@gmail.com>
2024-05-23 00:30:30 +0800

    :sparkles: Modified configuration file system

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'throw' => false,
         ],
 
+        'api_uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/api_data',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
