ed06ab28ed28bb7145b2ee39dcfef9c0b1b28efd
da-imran <ios.imrannn@gmail.com>
2024-06-01 20:24:57 +0800

    :sparkles: Modified carta organisasi view blade

--- resources/views/corporate/cartaOrganisasi.blade.php
+++ resources/views/corporate/cartaOrganisasi.blade.php
@@ -5,7 +5,12 @@
        <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     </head>
     <style>
-       
+       .image-organisasi{
+        width: 500px;
+        max-width: 900px;
+        margin: auto;
+        display: block;
+       }
     </style>
     <body>
         @include('index.indexTop')
@@ -13,6 +18,11 @@
             <div class="row">
               <div class="col">
                 <h6 class="text mt-3" style="font-size: 20px"><b>Carta Organisasi</b></h6>
+                <div class="col-md-12 mb-3">
+                  <div class="image-organisasi border">
+                    <img src="/images/masjid-organisasi.png" alt="Carta Organisasi">
+                  </div>
+                </div>
                 <div class="mt-5 mb-5">
                   <p> Halaman ini sedang dibangunkan</p>
                 </div>
